df93395925d4b086a5a19eb02143b1b1fe230424
mya-ake <make.app.ts@gmail.com>
2019-01-30 03:46:00 +0900

    update

--- global-components/VpccGridViewItem.vue
+++ global-components/VpccGridViewItem.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="vpcc-grid-view__item">
+  <router-link :to="to" class="vpcc-grid-view__item">
     <span class="vpcc-grid-view__item-name"><slot name="name"/></span>
     <slot />
-  </div>
+    <div class="vpcc-grid-view__item-front-layer"></div>
+  </router-link>
 </template>
 
 <script>
 export default {
   name: 'VpccGridViewItem',
+
+  props: {
+    to: {
+      type: String,
+      default: '',
+    },
+  },
 };
 </script>
