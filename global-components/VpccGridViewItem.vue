b3a716d436795986c81b8079b1724bc9f9d1592b
mya-ake <make.app.ts@gmail.com>
2019-01-30 03:29:29 +0900

    update

--- global-components/VpccGridViewItem.vue
+++ global-components/VpccGridViewItem.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="vpcc-grid-view__item"><slot /></div>
+  <div class="vpcc-grid-view__item">
+    <span class="vpcc-grid-view__item-name"><slot name="name"/></span>
+    <slot />
+  </div>
 </template>
 
 <script>
