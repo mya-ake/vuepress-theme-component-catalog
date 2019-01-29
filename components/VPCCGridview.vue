e9fd9fc3aadc79217e4a8be916dad8fff8b3b585
mya-ake <make.app.ts@gmail.com>
2019-01-30 02:46:26 +0900

    update

--- components/VPCCGridview.vue
+++ components/VPCCGridview.vue
@@ -4,6 +4,6 @@
 
 <script>
 export default {
-  name: 'VPCCGridview',
+  name: 'VpccGridView',
 };
 </script>
--- components/VPCCGridviewItem.vue
+++ components/VPCCGridviewItem.vue
@@ -4,6 +4,6 @@
 
 <script>
 export default {
-  name: 'VPCCGridviewItem',
+  name: 'VpccGridViewItem',
 };
 </script>
